e82a761ac4bffba0c2f1a37b9b107592bff517bb
tomifrick <tfrick@student.ethz.ch>
2018-11-15 13:20:29 +0100

    Update WSL.ps1

--- scripts/WSL.ps1
+++ scripts/WSL.ps1
@@ -1,30 +1,25 @@
-choco install -y Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'"
-
-#--- Ubuntu ---
-# TODO: Move this to choco install once --root is included in that package
-Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1804 -OutFile ~/Ubuntu.appx -UseBasicParsing
-Add-AppxPackage -Path ~/Ubuntu.appx
-# run the distro once and have it install locally with root user, unset password
-
 RefreshEnv
-Ubuntu1804 install --root
 Ubuntu1804 run apt update
 Ubuntu1804 run apt upgrade -y
 
-<#
-NOTE: Other distros can be scripted the same way for example:
+# install neovim
+Ubuntu1804 run apt-get install python-dev python-pip python3-dev python3-pip -y
+Ubuntu1804 run apt install software-properties-common -y
+Ubuntu1804 run apt-add-repository ppa:neovim-ppa/stable -y
+Ubuntu1804 run apt update
+Ubuntu1804 run apt install neovim -y
 
-#--- SLES ---
-# Install SLES Store app
-Invoke-WebRequest -Uri https://aka.ms/wsl-sles-12 -OutFile ~/SLES.appx -UseBasicParsing
-Add-AppxPackage -Path ~/SLES.appx
-# Launch SLES
-sles-12.exe
+Ubuntu1804 run sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+Ubuntu1804 run sudo update-alternatives --config vi
+Ubuntu1804 run sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+Ubuntu1804 run sudo update-alternatives --config vim
+Ubuntu1804 run sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+Ubuntu1804 run sudo update-alternatives --config editor
 
-# --- openSUSE ---
-Invoke-WebRequest -Uri https://aka.ms/wsl-opensuse-42 -OutFile ~/openSUSE.appx -UseBasicParsing
-Add-AppxPackage -Path ~/openSUSE.appx
-# Launch openSUSE
-opensuse-42.exe
-#>
+# install zsh
+Ubuntu1804 run apt install zsh
+Ubuntu1804 run chsh -s $(which zsh)
+# Oh my zsh (dont know if works)
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+Ubuntu1804 run apt install terminator
